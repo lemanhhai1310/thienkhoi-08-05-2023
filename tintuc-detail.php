3dac759c797bd2c69637a83fd8d1fc906be64b90
hailm <hai.lemanh.1310@gmail.com>
2023-05-19 11:41:35 +0700

    feat: page tintuc-detail.php

---
+++ tintuc-detail.php
@@ -0,0 +1,44 @@
+<?php $data["title"] = "Chi Tiết Tin tức"; ?>
+<?php $bodyClass = '' ?>
+<?php $menu_position = false ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-section home__section">
+    <div class="uk-container uk-container-small uk-text-center">
+        <h1 class="uk-h1 tintuc__titleDetail">It is a long established fact that a reader will be distracted by the readable</h1>
+        <div class="uk-flex uk-flex-middle uk-flex-center item__20-12">
+            <span class="home__blog__card__txtSmall">Admin</span>
+            <span class="home__blog__card__txtSmall">26 th2 2023</span>
+        </div>
+    </div>
+    <div class="uk-container item__40-20 uk-padding-remove">
+        <div class="uk-cover-container">
+            <img src="images/noimage.jpg" alt="" uk-cover="">
+            <canvas width="1280" height="480"></canvas>
+        </div>
+    </div>
+    <div class="uk-container uk-container-small item__40-20 uk-position-relative">
+        <article class="uk-article tintuc__article">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <img class="uk-width" src="images/noimage.jpg" alt="">
+            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <img class="uk-width" src="images/noimage.jpg" alt="">
+            <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </article>
+        <div uk-toggle="cls: uk-position-center-right-out; mode: media; media: @l" class="tintuc__position item__40-20 uk-child-width-auto uk-flex-center uk-grid-20 uk-child-width-1-1@m" uk-grid>
+            <div>
+                <a href="" class="tintuc__social tintuc__social--fb"></a>
+            </div>
+            <div>
+                <a href="" class="tintuc__social tintuc__social--tiktok"></a>
+            </div>
+            <div>
+                <a href="" class="tintuc__social tintuc__social--yt"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
